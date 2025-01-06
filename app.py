ca5ed60212229275fc63d1a7164258e904c6b9c5
channu dambal <152293657+Channu17@users.noreply.github.com>
2025-01-06 08:22:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 nltk.download('stopwords')
 
-with open('Credit Score/Default risk Analysis/log_reg.pkl', 'rb') as file:
+with open('Default risk Analysis/log_reg.pkl', 'rb') as file:
     model = pickle.load(file)
 
-model_path = 'Credit Score/Sentiment Analysis/my_model.keras'
+model_path = 'Sentiment Analysis/my_model.keras'
 sent_model = load_model(model_path)
 
-with open('Credit Score/Sentiment Analysis/tokenizer.pkl', 'rb') as file:
+with open('Sentiment Analysis/tokenizer.pkl', 'rb') as file:
     tokenizer = pickle.load(file)
 
 
